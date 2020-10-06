900b33a498991b440d37e540de78455040108a8c
andrademiguel <miguel4andrade@gmail.com>
2020-10-06 16:12:51 -0700

    wip

--- js/ex3.js
+++ js/ex3.js
@@ -1,12 +1,46 @@
-var fName= window.prompt(`Input your first name:`);
-var lName = window.prompt(`Input your last name:`);
-var scores = window.prompt(`Input your grades:`);
 
 const studentList = [
     {
-        fName:[],
-        lName:[],
-        scores:[]
-    }
+        firstName: "Allan",
+        lastName: "Able",
+        scores: [95, 85, 92, 98]
+    },
+    {
+        firstName: "Amy",
+        lastName: "Alexander",
+        scores: [80, 88, 100]
+    },
+    {
+        firstName: "Betty",
+        lastName: "Barns",
+        scores: [70, 80, 90, 100]
+    },
+    {
+        firstName: "Bob",
+        lastName: "Bones",
+        scores: [75, 85, 95, 85]
+    },
+    {
+        firstName: "Cindy",
+        lastName: "Chase",
+        scores: [95, 90, 92, 98]
+    },
+    {
+        firstName: "Charles",
+        lastName: "Chips",
+        scores: [88, 99, 90]
+    },
 ];
 
+
+
+const average = array => array.reduce((sum, value) => sum + value, 0) / array.length; 
+console.log(
+    studentList.filter(lastName).map(studentList => {
+    return {
+        firstName: studentList.firstName,
+        averageGrade: average(studentList.scores)
+    };
+})
+);
+//console.log(cLastNameResults);
